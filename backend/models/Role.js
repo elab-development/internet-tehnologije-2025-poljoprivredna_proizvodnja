dd58b9569665cbb25409ce3db0082885a70fbbb2
Marko Stamenkovic <stamenkovic.marko173@gmail.com>
2026-01-18 10:53:24 +0100

    Dodat Role

---
+++ backend/models/Role.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Role = sequelize.define("Role", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  }
+}, {
+  tableName: "roles",
+  timestamps: false
+});
+
+module.exports = Role;
