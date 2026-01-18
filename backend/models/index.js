158fc3908228da4c966415a1084b546dde5e0f53
Marko Stamenkovic <stamenkovic.marko173@gmail.com>
2026-01-18 12:00:04 +0100

    Dodat index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,11 +1,40 @@
-const sequelize = require("../config/database");
+const { Sequelize, DataTypes } = require('sequelize');
+const config = require('../config/config.json')['development'];
 
-const Role = require("./Role");
-const User = require("./User");
-const Field = require("./Field");
-const Crop = require("./Crop");
-const Production = require("./Production");
-const Expense = require("./Expense");
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+  host: config.host,
+  dialect: config.dialect
+});
 
-// export everything
-module.exports = { sequelize, Role, User, Field, Crop, Production, Expense };
+const Role = require('./role')(sequelize, DataTypes);
+const User = require('./user')(sequelize, DataTypes);
+const Field = require('./field')(sequelize, DataTypes);
+const Crop = require('./crop')(sequelize, DataTypes);
+const Production = require('./production')(sequelize, DataTypes);
+const Expense = require('./expense')(sequelize, DataTypes);
+
+// Relacije
+Role.hasMany(User, { foreignKey: 'roleId' });
+User.belongsTo(Role, { foreignKey: 'roleId' });
+
+User.hasMany(Field, { foreignKey: 'userId' });
+Field.belongsTo(User, { foreignKey: 'userId' });
+
+Field.hasMany(Crop, { foreignKey: 'fieldId' });
+Crop.belongsTo(Field, { foreignKey: 'fieldId' });
+
+Crop.hasMany(Production, { foreignKey: 'cropId' });
+Production.belongsTo(Crop, { foreignKey: 'cropId' });
+
+Field.hasMany(Expense, { foreignKey: 'fieldId' });
+Expense.belongsTo(Field, { foreignKey: 'fieldId' });
+
+module.exports = {
+  sequelize,
+  Role,
+  User,
+  Field,
+  Crop,
+  Production,
+  Expense
+};
