b1f7c938e3dd6965881ea729f68c7e8198553393
Marko Stamenkovic <stamenkovic.marko173@gmail.com>
2026-01-18 11:56:56 +0100

    Dodat userRouts.js

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+const auth = require('../middleware/auth');
+
+router.post('/register', userController.register);
+router.post('/login', userController.login);
+
+// primer rute za zaštitu
+router.get('/me', auth, async (req, res) => {
+  res.json({ userId: req.user.id });
+});
+
+module.exports = router;
