88bd9a74b32ebd79e5898d7c9a0a0eb20f097751
Marko Stamenkovic <stamenkovic.marko173@gmail.com>
2026-01-27 18:43:51 +0100

    Zavresen css

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -2,10 +2,45 @@ import React from 'react';
 
 export default function Dashboard() {
   return (
-    <div>
-      <h2>Dashboard</h2>
-      <p>Welcome to the Agriculture Management System</p>
-      <p>Use the navigation to manage Fields, Crops, Productions, and Expenses</p>
+    <div class='dash'>
+      <div class='bg'>
+        <h2 class='h2'>Početna strana</h2>
+      </div>
+      <div class='welc'>
+        <h3 class='h3'>Dobro došli na AgroPanel - Vaš pouzdan partner u proizvodnji kukuruza</h3>
+      </div>
+      <div class='text'>
+        <p>Kukuruz je jedna od najvažnijih kultura u poljoprivredi, a njegova proizvodnja predstavlja izazov koji zahteva znanje, iskustvo i pravu tehnologiju. U AgroPanel-u verujemo da svaki proizvođač može da postigne maksimalan prinos i kvalitet, uz pravilnu organizaciju i adekvatnu podršku.<b>Naš cilj</b> je da vam pružimo sve potrebne informacije na jednom mestu – od planiranja setve i uzgoja do berbe.</p>
+      </div> 
+      <div class='cards'>
+        <h2 class='c-h2'>Šta možete očekivati od nas :</h2>
+        <div class='card1'>
+          <h4>Stručni agronomski saveti</h4>
+          <p>Pripremili smo praktične i proverene preporuke za sve faze gajenja kukuruza – od pripreme zemljišta, izbora hibrida, setve, prihrane, zaštite do pravilne berbe. Sve informacije su napisane jasno i razumljivo, kako bi bile korisne i početnicima i iskusnim proizvođačima.</p>
+        </div>
+        <br></br>
+        <div class='card2'>
+          <h4>Zaštita i ishrana biljaka</h4>
+          <p>Kukuruz je kultura koja je osetljiva na bolesti, štetočine i nepovoljne vremenske uslove. Pomažemo vam da prepoznate najčešće probleme i primenite efikasne mere zaštite. Takođe, objavljujemo preporuke za pravilnu i ekonomičnu prihranu, kako biste povećali prinos bez nepotrebnih troškova.</p>
+        </div>
+        <br></br>
+        <div class='card3'>
+          <h4>Informacije o sortama i hibridima</h4>
+          <p>Izbor pravog hibrida je ključ uspešne proizvodnje. Ovde ćete pronaći detaljne opise različitih sorti, njihove prednosti, adaptabilnost na različite uslove i preporuke za optimalne rezultate.</p>
+        </div>
+        <br></br>
+        <div class='card4'>
+          <h4>Tehnologija i inovacije u poljoprivredi</h4>
+          <p>Pratimo nove tehnologije i trendove u proizvodnji kukuruza, kao što su precizna poljoprivreda, navodnjavanje, savremena mehanizacija i digitalni alati. Cilj nam je da vam pokažemo kako da proizvodnju učinite efikasnijom i održivijom.</p>
+        </div>
+      </div>
+      <br></br>
+      <div class='end'>
+        <h3>Započnite već danas!</h3>
+        <p>Bilo da ste početnik ili iskusan proizvođač, na AgroPanel-u ćete pronaći korisne informacije koje će vam pomoći da poboljšate prinos i kvalitet kukuruza. Pratite naše savete, primenjujte preporuke i zajedno sa nama postignite bolje rezultate na polju.</p>
+      </div>
     </div>
+    
+    
   );
 }
